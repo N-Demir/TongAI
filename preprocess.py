eeb5e5d5b01ad035847f1ab3f513a5bc077ad11a
Nikita Demir <nikitde1@gmail.com>
2019-03-04 20:25:15 -0800

    Added Naive Bayes

--- preprocess.py
+++ preprocess.py
@@ -4,6 +4,10 @@ Run once to get nicely formatted data
 IMPORTANT: You need to first create a folder 'data' and inside there a folder called 'processed' (feel free to change)
 		   Then, move the 'neuro_protocol_sent_021419.xlsx' file into data and run this script.
 
+The final representation is:
+
+age||gender||protocol_class_number||reason_for_visit
+
 """
 
 import numpy as np
@@ -68,11 +72,34 @@ inFile.close()
 
 #### Turning them into classes
 
-# print('Creating the classes')
+print('Creating the classes')
 
-# with open(save_folder_path + 'no_duplicates_processed.csv','r') as inFile:
+inFile = open(save_folder_path + 'no_duplicates_processed.csv','r')
 
-# 	for line in inFile:
-# 		split = re.split(DELIMITER, line)
+outFile = open(save_folder_path + 'final.csv','w')
 
-# 		print(split[2])
+classes = []
+class_length = {}
+
+for line in inFile:
+	split = line.split(DELIMITER)
+
+	class_name = split[2].lower()
+
+	if class_name in classes:
+		index = classes.index(class_name)
+		class_length[class_name] += 1
+	else:
+		classes.append(class_name)
+		index = len(classes) - 1
+		class_length[class_name] = 1
+
+	outFile.write(split[0] + DELIMITER + split[1] + DELIMITER + str(index) + DELIMITER + split[3])
+
+print('Created classes are: {}'.format(classes))
+print('Total number: {}'.format(len(class_length)))
+print('For histogram purposes {}'.format(class_length))
+
+outFile.close()
+
+inFile.close()
