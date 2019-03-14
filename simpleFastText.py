50c294f4c150c44834cc0d6684d25758c4bb9980
Nikita Demir <nikitde1@gmail.com>
2019-03-13 22:17:35 -0700

    Added a simple CNN

--- simpleFastText.py
+++ simpleFastText.py
@@ -26,10 +26,10 @@ With embed_dim = 100 and 25002 in vocabulary:
 | Epoch: 20 | Train Loss: 0.456| Train Acc: 88.70% | Val. Loss: 1.639 | Val. Acc: 77.60% 
 
 """
-
 import torch
 from torchtext import data
 from torchtext import datasets
+import data_loader
 import random
 import torch.nn as nn
 import torch.nn.functional as F
@@ -83,6 +83,7 @@ class FastText(nn.Module):
         pooled = F.avg_pool2d(embedded, (embedded.shape[1], 1)).squeeze(1) 
         
         #pooled = [batch size, embedding_dim]
+
         # assert(pooled.shape == (BATCH_SIZE, EMBEDDING_DIM))
         logits = self.fc(pooled)
 
@@ -153,7 +154,7 @@ def main():
         return x
 
     # Load the data-set
-    TEXT, train_itr, valid_itr, test_itr = load_data(generate_bigrams)
+    TEXT, train_itr, valid_itr, test_itr = data_loader.load_data(generate_bigrams)
 
     input_dim = len(TEXT.vocab)
 
@@ -175,8 +176,6 @@ def main():
         valid_loss, valid_acc = evaluate(model, valid_itr, loss_function)
         
         print(f'| Epoch: {epoch:02} | Train Loss: {train_loss:.3f}| Train Acc: {train_acc*100:.2f}% | Val. Loss: {valid_loss:.3f} | Val. Acc: {valid_acc*100:.2f}% ')
-        # saveMetrics('train', train_acc, train_loss, train_prec, train_recall, train_f_score, epoch)
-        # saveMetrics('valid', valid_acc, valid_loss, valid_prec, valid_recall, valid_f_score, epoch)
         if (epoch % EPOCH_SAVE == 0):
             saveModel(model, epoch)
 
@@ -215,124 +214,6 @@ def saveModel(model, epoch):
 
     state = model.state_dict()
     torch.save(state, model_path)
-
-def saveMetrics(prefix, accuracy, loss, precisions, recalls, f1_scores, epoch):
-    real_precision, fake_precision = precisions
-    real_recall, fake_recall = recalls
-    real_f1_score, fake_f1_score = f1_scores
-
-    path = setupCheckpoints()
-
-    accuracy_path = path / '{}-accuracy.txt'.format(prefix)
-    loss_path = path / '{}-loss.txt'.format(prefix)
-
-    real_precision_path = path / '{}-real-precision.txt'.format(prefix)
-    fake_precision_path = path / '{}-fake-precision.txt'.format(prefix)
-    real_recall_path = path / '{}-real-recall.txt'.format(prefix)
-    fake_recall_path = path / '{}-fake-recall.txt'.format(prefix)
-    real_f1_score_path = path / '{}-real-f1_score.txt'.format(prefix)
-    fake_f1_score_path = path / '{}-fake-f1_score.txt'.format(prefix)
-
-    def writeMetric(metric_path, value):
-        with open(metric_path, 'a+') as f:
-            f.write('{},{}\n'.format(epoch, value))
-
-    writeMetric(accuracy_path, accuracy)
-    writeMetric(loss_path, loss)
-    writeMetric(real_precision_path, real_precision)
-    writeMetric(fake_precision_path, fake_precision)
-    writeMetric(real_recall_path, real_recall)
-    writeMetric(fake_recall_path, fake_recall)
-    writeMetric(real_f1_score_path, real_f1_score)
-    writeMetric(fake_f1_score_path, fake_f1_score)
-
-###### DATA LOADING STUFF
-
-
-import spacy
-from torchtext.data import Field, LabelField
-from torchtext.data import TabularDataset
-from torchtext.data import Iterator, BucketIterator
-from torchtext import datasets
-import torch
-from torch.nn import functional as F
-import random
-
-# English language model
-# Though we should play potentially with different
-# language models
-spacy_en = spacy.load('en')
-
-path = 'data/processed/final.csv'
-BATCH_SIZE = 64
-
-# Do this for testing
-# To see if we match the results from online
-#SEED = 1234
-SEED = 229
-TRAIN_VAL_TEST_SPLIT = [0.7, 0.15, 0.15]
-
-# Sets the random number generator of torch
-#torch.manual_seed(SEED)
-#torch.cuda.manual_seed(SEED)
-# May want to play with this for reproducability
-#torch.backends.cudnn.deterministic = True
-
-def tokenizer(text): # create a tokenizer function
-    # Consider using tok.lemma to lemmatize the
-    # vocabulary rather than true words
-
-    # We can also consider removing stop words!!
-
-    # Consider appending potentially named entity tags??
-    return [tok.text for tok in spacy_en.tokenizer(text)]
-
-# Note that now everything is tsv but would like json!!
-def load_data(preprocessing=None):
-    # Fields for the dataset
-    # The actual review message
-
-    #TEXT = Field(tokenize='spacy') # -- Old way, unclear exactly what language model is used
-    TEXT = Field(sequential=True, tokenize=tokenizer, lower=True, preprocessing=preprocessing)
-    LABEL = LabelField(dtype=torch.float)
-
-    # Get the entire dataset that we will then split
-    data = TabularDataset(
-        path=path, format='csv',
-        fields=[('age', None), ('gender', None), ('text', TEXT), ('label', LABEL)],
-        csv_reader_params={'delimiter':"|", 'quotechar': "\""})
-
-    # We should probabily look at the proportion of fake to non fake in each of these
-    # set to make sure it is fairly even. Though probabilistically it should be I suppose
-    train_data, valid_data, test_data = data.split(split_ratio=TRAIN_VAL_TEST_SPLIT, random_state=random.seed(SEED))
-    #valid_data, test_data = test_data.split(split_ratio=VAL_TEST_SPLIT, random_state=random.seed(SEED))
-
-    print ('Size of train set: ' + str(len(train_data.examples)))
-    print ('Size of val / test: ' + str(len(valid_data.examples)))
-
-    '''
-    # Try loading in the IMB dataset to label pos or negative
-    train_data, test_data = datasets.IMDB.splits(TEXT, LABEL)
-    # Get train/valid split!!
-    train_data, valid_data = train_data.split(random_state=random.seed(SEED))
-    '''
-
-    # Now we need to build the vocab for our actual data
-    # Here we will use the pre-trained word vetors from "glove.6b.100"
-    TEXT.build_vocab(train_data, max_size=25000, vectors="glove.6B.100d")
-    LABEL.build_vocab(train_data)
-
-    # Print stuff for sanity checks
-    print ('Size of the vocab: ' + str(len(TEXT.vocab)))
-    print ("Vector size of Text Vocabulary: ", TEXT.vocab.vectors.size())
-    print ("Label Length: " + str(len(LABEL.vocab)))
-
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-    train_itr, valid_itr, test_itr = BucketIterator.splits((train_data, valid_data, test_data),
-        batch_size=BATCH_SIZE, device=device, sort_key=lambda x: len(x.text))
-
-    return TEXT, train_itr, valid_itr, test_itr
 
 if __name__ == '__main__':
     main()
